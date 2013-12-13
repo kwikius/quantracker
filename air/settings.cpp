cc20519065709db642380335bcd7477e878e896c
skyscraper <kwikius@yahoo.com>
2013-12-13 22:00:00 +0000

    separated ZAPP1 protocol for FrSky out to separate file

---
+++ air/settings.cpp
@@ -0,0 +1,3 @@
+#include "settings.hpp"
+
+settings::altitude_t settings::altitude_src = settings::altitude_t::baro_alt;
---
+++ air/settings.hpp
@@ -0,0 +1,11 @@
+#ifndef QUANTRACKER_AIR_SETTINGS_HPP_INCLUDED
+#define QUANTRACKER_AIR_SETTINGS_HPP_INCLUDED
+
+struct settings{
+
+   enum class altitude_t {gps_alt, baro_alt};
+
+   static altitude_t altitude_src;
+};
+
+#endif // QUANTRACKER_AIR_SETTINGS_HPP_INCLUDED
