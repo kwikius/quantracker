4b116b4e67b1e8e514fb23f7012374b7a77e27ee
skyscraper <kwikius@yahoo.com>
2014-10-28 13:53:18 +0000

    move air/ module to air/v1 and create osd dir

--- ground/tracker/v1/flash/flash_type_tags.hpp
+++ ground/tracker/v1/flash/flash_type_tags.hpp
@@ -3,28 +3,29 @@
 
 #include <quan/three_d/vect.hpp>
 
-// local
+// each type in flash needs a typeid
+// these are zero based and are used as indexes
+// in array
 enum flash_type_tags { Vect3F, Bool};
 
-// app level create unique integer tags for each type in the eeprom structure
+// app level create unique integer tags for each type in the flash
 // accessible using these templates
 template <typename T> struct type_to_id;
+template <uint32_t id> struct id_to_type;
 
 template <> struct type_to_id<quan::three_d::vect<float> > {
    static constexpr uint32_t value = flash_type_tags::Vect3F;
 };
-template <> struct type_to_id<bool> {
-   static constexpr uint32_t value = flash_type_tags::Bool;
-};
-
-template <uint32_t id> struct id_to_type;
-
 template <> struct id_to_type<flash_type_tags::Vect3F>{
    typedef quan::three_d::vect<float> type;
 };
-
+//---------
+template <> struct type_to_id<bool> {
+   static constexpr uint32_t value = flash_type_tags::Bool;
+};
 template <> struct id_to_type<flash_type_tags::Bool>{
    typedef bool type;
 };
+//---
 
 #endif // QUANTRACKER_FLASH_FLASH_TYPE_TAGS_HPP_INCLUDED
