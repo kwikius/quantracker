0f800ca58412e07129e361f0906564fa3b45ccad
skyscraper <kwikius@yahoo.com>
2015-03-29 17:54:05 +0100

    moved some flash var type stuff to quan-trunk

--- air/osd/flash.cpp
+++ air/osd/flash.cpp
@@ -15,179 +15,16 @@
  along with this program. If not, see <http://www.gnu.org/licenses/>
 */
 
-/*
- Flash variables implementation.
- Flash variables of various types can be defined.
-
-*/
-
 #include <cstring>
 #include <quan/three_d/vect.hpp>
 #include <quan/stm32/flash.hpp>
-#include <quan/stm32/flash/flash_convert/vect3f.hpp>
-#include <quan/stm32/flash/flash_convert/vect3i32.hpp>
-#include <quan/stm32/flash/flash_convert/bool.hpp>
-#include <quan/stm32/flash/flash_convert/float.hpp>
-#include <quan/stm32/flash/flash_convert/int.hpp>
-#include <quan/stm32/detail/flash.hpp>
+#include <quan/user.hpp>
 #include <quan/stm32/flash/flash_error.hpp>
 
 namespace {
 
-   // The enum holds numeric ids , one for each different type in the flash vars 
-   // In practise can prob use less types 1 int and 1 uint say
-   // (TODO redo templates to reuse common stuff and save code bloat)
-   // Comment out which you dont need
-   enum flash_type_tags { 
-         Bool 
-         ,Float
-         ,Int8
-         ,Int16
-         ,Int32
-         ,Uint8
-         ,Uint16
-         ,Uint32
-         ,Vect3I32  
-         ,Vect3F
-         ,NumTypeTags
-     }; // Use NumTypeTags to check array sizes etc
-
-   // use these local templates to map types to ids and vice versa
-   template <typename T> struct type_to_flash_id;
-   template <uint32_t id> struct flash_id_to_type;
-
-    //--------- for Bool
-   template <> struct type_to_flash_id<bool> {
-      static constexpr uint32_t value = flash_type_tags::Bool;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Bool>{
-      typedef bool type;
-   };
-
-   //--------- for Float
-   template <> struct type_to_flash_id<float> {
-      static constexpr uint32_t value = flash_type_tags::Float;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Float>{
-      typedef float type;
-   };
-
-    //--------- for Int8
-   template <> struct type_to_flash_id<int8_t> {
-      static constexpr uint32_t value = flash_type_tags::Int8;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Int8>{
-      typedef int8_t type;
-   };
-
-    //--------- for Int16
-   template <> struct type_to_flash_id<int16_t> {
-      static constexpr uint32_t value = flash_type_tags::Int16;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Int16>{
-      typedef int16_t type;
-   };
-   
-    //--------- for Int32
-   template <> struct type_to_flash_id<int32_t> {
-      static constexpr uint32_t value = flash_type_tags::Int32;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Int32>{
-      typedef int32_t type;
-   };
-
-    //--------- for Uint8
-   template <> struct type_to_flash_id<uint8_t> {
-      static constexpr uint32_t value = flash_type_tags::Uint8;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Uint8>{
-      typedef uint8_t type;
-   };
-
-    //--------- for Uint16
-   template <> struct type_to_flash_id<uint16_t> {
-      static constexpr uint32_t value = flash_type_tags::Uint16;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Uint16>{
-      typedef uint16_t type;
-   };
-   
-    //--------- for Uint32
-   template <> struct type_to_flash_id<uint32_t> {
-      static constexpr uint32_t value = flash_type_tags::Uint32;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Uint32>{
-      typedef uint32_t type;
-   };
-
-   //--------- for Vect3I32
-   template <> struct type_to_flash_id<quan::three_d::vect<int32_t> > {
-      static constexpr uint32_t value = flash_type_tags::Vect3I32;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Vect3I32>{
-      typedef quan::three_d::vect<int32_t> type;
-   };
-
-   //--------- for Vect3F
-   template <> struct type_to_flash_id<quan::three_d::vect<float> > {
-      static constexpr uint32_t value = flash_type_tags::Vect3F;
-   };
-   template <> struct flash_id_to_type<flash_type_tags::Vect3F>{
-      typedef quan::three_d::vect<float> type;
-   };
-  
-}
-
-namespace quan{ namespace stm32{ namespace flash{
-
-      // per app ids for types. Since types may be arranged different ways this is 
-      // for the app to define as here
-      // creeky but works!
-
-      // implement the template function
-      template <typename T> uint32_t get_flash_typeid(){ return get_flash_typeid_impl<T>::apply();}
-
-      // provide specialisations  done like this so that order change is picked up
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<bool>::apply() 
-         {return type_to_flash_id<bool>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<float>::apply() 
-         {return type_to_flash_id<float>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<int8_t>::apply() 
-         {return type_to_flash_id<int8_t>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<int16_t>::apply() 
-         {return type_to_flash_id<int16_t>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<int32_t>::apply() 
-         {return type_to_flash_id<int32_t>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<uint8_t>::apply() 
-         {return type_to_flash_id<uint8_t>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<uint16_t>::apply() 
-         {return type_to_flash_id<uint16_t>::value;}
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<uint32_t>::apply() 
-         {return type_to_flash_id<uint32_t>::value;}
-
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<quan::three_d::vect<int32_t> >::apply() 
-         {return type_to_flash_id<quan::three_d::vect<int32_t> >::value;}
-
-      uint32_t quan::stm32::flash::get_flash_typeid_impl<quan::three_d::vect<float> >::apply() 
-         {return type_to_flash_id<quan::three_d::vect<float> >::value;}
-     
-      // and these are explicit instantiations of these functions for the linker to find 
-      template  uint32_t get_flash_typeid<bool>();
-      template  uint32_t get_flash_typeid<float>();
-      template  uint32_t get_flash_typeid<int8_t>();
-      template  uint32_t get_flash_typeid<int16_t>();
-      template  uint32_t get_flash_typeid<int32_t>();
-      template  uint32_t get_flash_typeid<uint8_t>();
-      template  uint32_t get_flash_typeid<uint16_t>();
-      template  uint32_t get_flash_typeid<uint32_t>();
-      template  uint32_t get_flash_typeid<quan::three_d::vect<float> >();
-      template  uint32_t get_flash_typeid<quan::three_d::vect<int32_t> >();
-}}}
-
-namespace {
-
    // The symbol table for this app. Provides the implementation of the 
-   // various data required by the symbol table
+   // various virtual functions data required by the symbol table
    struct app_symtab_t : quan::stm32::flash::symbol_table {
       app_symtab_t() {}
       ~app_symtab_t() {}
@@ -197,83 +34,13 @@ namespace {
       const char* get_name (int32_t symbol_index)const;
       const char* get_info (int32_t symbol_index) const;
       bool get_readonly_status(int32_t symbol_index,bool & result)const;
-      pfn_text_to_bytestream get_text_to_bytestream_fun( int32_t symbol_index) const;
-      pfn_bytestream_to_text get_bytestream_to_text_fun( int32_t symbol_index) const;
       bool get_typeid(int32_t symbol_index, uint32_t & dest) const;
       bool init() const;
       pfn_check_function get_check_function(int32_t symbol_index)const;
    };
-//################### 
-
-
-   // An Array of function pointers 
-   // for converting a type as text to a bytestream
-   // indexed by id in the flash_type_tags enum
-   // The order and number 
-   // must be the same as the flash_type_tags enum above
-   // The use of the flash_id_to_type index hopefully helps!
-   constexpr quan::stm32::flash::symbol_table::pfn_text_to_bytestream text_to_bytestream[] =
-   {
-      &quan::stm32::flash::flash_convert<flash_id_to_type<0>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<1>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<2>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<3>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<4>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<5>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<6>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<7>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<8>::type>::text_to_bytestream
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<9>::type>::text_to_bytestream
-   };
-   static_assert ((sizeof(text_to_bytestream) 
-         / sizeof(quan::stm32::flash::symbol_table::pfn_text_to_bytestream)) 
-      == flash_type_tags::NumTypeTags,"text_to_bytestream invalid number of elements");
-
-   // Array describing the size of a type in flash
-   // indexed by id in the flash_type_tags enum
-   // the order and number must be the same as the flash_type_tags enum
-   // above.
-   // There must be a separate entry for each different flash type
-   constexpr uint32_t type_tag_to_size[] =
-   {
-      sizeof (flash_id_to_type<0>::type)
-      ,sizeof (flash_id_to_type<1>::type) 
-      ,sizeof (flash_id_to_type<2>::type)
-      ,sizeof (flash_id_to_type<3>::type)
-      ,sizeof (flash_id_to_type<4>::type) 
-      ,sizeof (flash_id_to_type<5>::type)
-      ,sizeof (flash_id_to_type<6>::type)
-      ,sizeof (flash_id_to_type<7>::type) 
-      ,sizeof (flash_id_to_type<8>::type)
-      ,sizeof (flash_id_to_type<9>::type)
-   };
-
-   static_assert ((sizeof(type_tag_to_size) / sizeof(uint32_t))
-      == flash_type_tags::NumTypeTags,"type_tag_to_size invalid number of elements");
-    
-   // array of function pointers to convert a bytestream to text (user represntation)
-   // indexed by id in the flash_type_tags enum
-   // the order and number must be the same as the flash_type_tags enum
-   // Can be by type, not by object, since
-   // it is assumed that only valid object values have been stored in Flash.
-   constexpr quan::stm32::flash::symbol_table::pfn_bytestream_to_text bytestream_to_text[] =
-   {
-      &quan::stm32::flash::flash_convert<flash_id_to_type<0>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<1>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<2>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<3>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<4>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<5>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<6>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<7>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<8>::type>::bytestream_to_text
-      ,&quan::stm32::flash::flash_convert<flash_id_to_type<9>::type>::bytestream_to_text
-   };
-   static_assert ((sizeof(bytestream_to_text) 
-         / sizeof(quan::stm32::flash::symbol_table::pfn_bytestream_to_text)) 
-      == flash_type_tags::NumTypeTags,"bytestream_to_text invalid number of elements");
 
 // ####################### access to types via the lookup name ####################
+   // Not required for your implementation
    // This structure is just used to map the type of each flash variable to its name
    // for use by the EE_SYMTAB_ENTRY macro below. The order isnt important
    // except for neatness !
@@ -350,7 +117,7 @@ namespace {
    // Use the macro to add flash_symtab_entry_t entries for the names symbol table below
    #define EE_SYMTAB_ENTRY(Name, CheckFun,Info, Readonly) { \
             #Name, \
-            type_to_flash_id<flash_variable_type::Name >::value ,\
+            quan::stm32::flash::type_to_flash_id<flash_variable_type::Name >::value ,\
             CheckFun, \
             Info ,\
             Readonly \
@@ -375,20 +142,23 @@ namespace {
    #undef EE_SYMTAB_ENTRY
  
    // requires valid symbol_index -- not checked
-   constexpr uint16_t get_type_index (int32_t symbol_index)
-   {
-      return names[static_cast<uint16_t>(symbol_index)].type_tag;
-   }
+//   constexpr uint16_t get_type_index (int32_t symbol_index)
+//   {
+//      return names[static_cast<uint16_t>(symbol_index)].type_tag;
+//   }
    
-   // get size of a type by index
-   // requires valid type_index -- not checked
-   constexpr uint16_t get_type_size (uint16_t typeidx)
+   uint16_t get_type_size (uint16_t typeidx)
    {
-      return type_tag_to_size[typeidx];
+      return app_symtab_t::type_tag_to_size[typeidx];
    }
+  
     
    //instantiate the application symbol table
    app_symtab_t app_symtab;
+
+    // get size of a type by index
+   // requires valid type_index -- not checked
+
 
 } // namespace
 
@@ -436,7 +206,8 @@ bool app_symtab_t::get_readonly_status (int32_t symbol_index,bool & result)const
 uint16_t app_symtab_t::get_symbol_storage_size (int32_t symbol_index) const
 {
     if (this->is_valid_symbol_index(symbol_index)) {
-      return get_type_size (get_type_index (symbol_index));
+      return get_type_size (names[static_cast<uint16_t>(symbol_index)].type_tag);
+      //return get_type_size (get_type_index (symbol_index));
     }else{
       return 0U;
     }
@@ -447,6 +218,9 @@ uint16_t app_symtab_t::get_symtable_size() const
    return sizeof (names)/sizeof (flash_symtab_entry_t);
 }
 
+/*
+ slow but who cares really ?
+*/
 int32_t app_symtab_t::get_index (const char* symbol_name)const
 {
       int32_t count = 0;
@@ -469,70 +243,12 @@ const char* app_symtab_t::get_name (int32_t symbol_index)const
    }
 }
 
-// requires valid symbol_index -- not checked
-app_symtab_t::pfn_text_to_bytestream app_symtab_t::get_text_to_bytestream_fun (int32_t symbol_index)const
-{
-   return text_to_bytestream[get_type_index (symbol_index) ];
-}
-
-// coming out data assumed ok. add checks
-// requires valid symbol_index -- not checked
-app_symtab_t::pfn_bytestream_to_text app_symtab_t::get_bytestream_to_text_fun (int32_t symbol_index)const
-{
-   return bytestream_to_text[get_type_index (symbol_index) ];
-}
-
-namespace {
- 
-/*
-See if flash page has been written.
- Currently (with Linux version of ST-Link), the default 
- after uploading is to clear verything to 0
-  so starting from 0 or each byte with the sum.
-  if reult is 0 then page is empty
-*/
-uint8_t flash_check_page (uint8_t n)
-{
-   uint8_t sum = 0x00;
-   
-   volatile uint8_t * pn 
-      = (volatile uint8_t*)quan::stm32::flash::detail::get_page_address (n);
-   uint32_t  pn_size = quan::stm32::flash::detail::get_page_size (n);
-   if (pn_size ==0) {
-      quan::user_message ("invalid flash page\n");
-      return 0;
-   }
-   for (uint32_t i = 0; i < pn_size; ++i) {
-      sum |= *pn;
-      ++pn;
-   }
-   return sum ;
-}
- 
-uint8_t flash_check()
-{
-   return flash_check_page(1) | flash_check_page(2);
-}
-
-} //namespace
- 
 // defined in symbology.cpp
 bool init_values_from_flash();
 bool app_symtab_t::init()const 
 {
-   if (flash_check() == 0x00) {
-   
-     // quan::user_message ("unwritten flash detected... initialising\n");
-     
-      if (! quan::stm32::flash::detail::erase (1)) {
-         quan::error (quan::detail::function_id::stm32_flash_symtab_init,quan::detail::stm32_flash_erase_page_failed);
-         return false ;
-      }
-      if (! quan::stm32::flash::detail::erase (2)) {
-         quan::error(quan::detail::function_id::stm32_flash_symtab_init,quan::detail::stm32_flash_erase_page_failed);
-         return false;
-      }
-      //quan::user_message ("...flash erased OK\n");
+   if (!flash_init()){
+      return false;
    }
    return init_values_from_flash();
 }
