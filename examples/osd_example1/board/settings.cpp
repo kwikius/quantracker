ef0b7c5d6a025dfa9cefa6a03ba3fd3af6f71aee
skyscraper <kwikius@yahoo.com>
2015-04-18 18:31:17 +0100

    builds ok with some cruft removed but mot uploaded and tested yet

--- examples/osd_example1/board/settings.cpp
+++ examples/osd_example1/board/settings.cpp
@@ -16,17 +16,6 @@
  */
 #include "settings.hpp"
 
-#if 0 
-/* change BOOT1 option to allow use of serial port to program.
-If one is written then all must be rewritten
-*/
-#if defined QUAN_STM32F0
-extern uint32_t const user_options1 __attribute__ ((section ("processor_options1")))  = 0x10EF55AA;
-extern uint32_t const user_options2 __attribute__ ((section ("processor_options2")))  = 0x00FF00FF;
-extern uint32_t const user_options3 __attribute__ ((section ("processor_options3")))  = 0x00FF00FF;
-#endif
-#endif
-
 settings::altitude_t settings::altitude_src 
    = settings::altitude_t::baro_alt;
 
