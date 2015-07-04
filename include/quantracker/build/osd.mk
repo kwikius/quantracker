3414df30ce076fe12ca42cb983c784dcccc42a44
skyscraper <kwikius@yahoo.com>
2015-07-04 21:00:43 +0100

    add common build stuff in one file .. we will see

---
+++ include/quantracker/build/osd.mk
@@ -0,0 +1,110 @@
+
+HAVE_DEPENDENCIES_FILE := $(shell if test -f "../../Dependencies.mk"; then echo "True"; fi)
+
+ifeq ($(HAVE_DEPENDENCIES_FILE), )
+  quantracker-make-help:
+	@echo ' '
+	@echo '   ########## HELP - OSD firmware build needs more info ############'
+	@echo '   #                                                               #'
+	@echo '   #            Hi. Welcome to quantracker / air / OSD.            #'
+	@echo '   #                                                               #'
+	@echo '   #            To build the OSD firmware, you need to             #'
+	@echo '   #            create a Dependencies.mk file.                     #'
+	@echo '   #                                                               #'
+	@echo '   #            Please read "Sample-Dependencies.mk" .             #'
+	@echo '   #            in this directory for further Details.             #                                                          #'
+	@echo '   #                                                               #'
+	@echo '   #################################################################'
+	@echo ' '	
+else
+# need the sourcedir for freertos compile
+
+# You will need a custom Dependencies.mk
+include ../../Dependencies.mk
+
+###############################################################
+ifeq ($(TOOLCHAIN_PREFIX), )
+$(error "TOOLCHAIN_PREFIX must be defined to the path to the gcc-arm compiler - see README.")
+endif
+
+ifeq ($(TOOLCHAIN_GCC_VERSION), )
+$(error "TOOLCHAIN_GCC_VERSION must be defined to the gcc-arm compiler version - see README.")
+endif
+
+ifeq ($(QUAN_INCLUDE_PATH), )
+$(error "QUAN_INCLUDE_PATH must be defined to the path to the quan library - see README.")
+endif
+
+ifeq ($(FREE_RTOS_DIR), )
+$(error "FREE_RTOS_DIR must be defined to the path to the FreeRTOS library - see README.")
+endif
+
+ifeq ($(STM32_STD_PERIPH_LIB_DIR), )
+$(error "STM32_STD_PERIPH_LIB_DIR must be defined to the path to the STM32 Std peripherals library - see README.")
+endif
+
+STM32_INCLUDES = $(STM32_STD_PERIPH_LIB_DIR)CMSIS/Include \
+$(STM32_STD_PERIPH_LIB_DIR)CMSIS/Device/ST/STM32F4xx/Include \
+$(STM32_STD_PERIPH_LIB_DIR)STM32F4xx_StdPeriph_Driver/inc
+
+RTOS_INCLUDES = \
+$(FREE_RTOS_DIR)Source/include/ \
+$(FREE_RTOS_DIR)Source/portable/GCC/ARM_CM4F \
+./
+
+TARGET_PROCESSOR = STM32F4
+
+INIT_LIB_PREFIX = $(TOOLCHAIN_PREFIX)/lib/gcc/arm-none-eabi/$(TOOLCHAIN_GCC_VERSION)/armv7e-m/fpu/
+INIT_LIBS = $(INIT_LIB_PREFIX)crti.o $(INIT_LIB_PREFIX)crtn.o 
+
+STATIC_LIBRARY_PATH = ../../lib/osd/
+
+ifeq ($(OPTIMISATION_LEVEL), )
+OPTIMISATION_LEVEL = O3
+endif
+
+ifeq ( $(CFLAG_EXTRAS), )
+CFLAG_EXTRAS = -fno-math-errno
+endif
+
+#required for Ubuntu 12.x placid as system headers have been put in strange places
+# these have beeen defined to thos in my bash .profile
+CPLUS_INCLUDE_PATH=
+C_INCLUDE_PATH=
+LIBRARY_PATH=
+
+CC      = $(TOOLCHAIN_PREFIX)bin/arm-none-eabi-g++
+CC1     = $(TOOLCHAIN_PREFIX)bin/arm-none-eabi-gcc
+LD      = $(TOOLCHAIN_PREFIX)bin/arm-none-eabi-g++
+CP      = $(TOOLCHAIN_PREFIX)bin/arm-none-eabi-objcopy
+OD      = $(TOOLCHAIN_PREFIX)bin/arm-none-eabi-objdump
+SIZ     = $(TOOLCHAIN_PREFIX)bin/arm-none-eabi-size
+  
+# specific flags for stm32f4
+DEFINES = QUAN_STM32F4 QUAN_FREERTOS $(TELEMETRY_DIRECTION) STM32F40_41xxx \
+QUAN_OSD_SOFTWARE_SYNCSEP HSE_VALUE=8000000 QUAN_OSD_BOARD_TYPE=4
+
+PROCESSOR_FLAGS = -march=armv7e-m -mtune=cortex-m4 -mhard-float -mthumb \
+-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mthumb -mfloat-abi=hard
+
+INCLUDES = $(STM32_INCLUDES) $(QUAN_INCLUDE_PATH) $(QUANTRACKER_INCLUDE_PATH) \
+$(RTOS_INCLUDES)
+
+INCLUDE_ARGS = $(patsubst %,-I%,$(INCLUDES))
+
+DEFINE_ARGS = $(patsubst %,-D%,$(DEFINES))
+
+CFLAGS  = -Wall -Wdouble-promotion -std=gnu++11 -fno-rtti -fno-exceptions -c -g \
+-$(OPTIMISATION_LEVEL) $(DEFINE_ARGS) $(INCLUDE_ARGS) $(PROCESSOR_FLAGS) \
+ $(CFLAG_EXTRAS) -fno-math-errno -Wl,-u,vsprintf -lm -fdata-sections -ffunction-sections
+
+LFLAGS  = -T$(LINKER_SCRIPT) -$(OPTIMISATION_LEVEL) -nostartfiles -nodefaultlibs \
+ $(PROCESSOR_FLAGS) --specs=nano.specs $(CFLAG_EXTRAS) -Wl,--gc-sections
+
+CPFLAGS = -Obinary
+ODFLAGS = -d 
+
+static_libraries = $(patsubst %,$(STATIC_LIBRARY_PATH)%,$(static_library_files))
+
+#deps conditional
+endif
