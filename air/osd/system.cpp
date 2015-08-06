a542a03344d3d063e5b2c4f6d20070cf9bd14cbe
skyscraper <kwikius@yahoo.com>
2015-08-06 09:27:40 +0100

    add array new and elete and __cxa_guard acquire release to system.cpp for c++ system initialisation

--- air/osd/system.cpp
+++ air/osd/system.cpp
@@ -15,3 +15,30 @@ extern "C" void * pvPortMalloc(size_t n);
 
 void operator delete (void* pv){ vPortFree(pv);}
 void* operator new (unsigned int n){ return pvPortMalloc(n);}
+
+void * operator new[](size_t n)
+{
+    return pvPortMalloc( ((n>0)?n:1) );
+}
+
+void operator delete[](void * ptr)
+{
+    if (ptr) vPortFree(ptr);
+}
+
+// from Ardupilot/libraries/AP_Common/c++.cpp
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+int __cxa_guard_acquire(__guard *g)
+{
+    return !*(char *)(g);
+};
+
+void __cxa_guard_release (__guard *g)
+{
+    *(char *)g = 1;
+};
+
+void __cxa_guard_abort (__guard *) 
+{
+};
