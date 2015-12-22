6cbdeb1ec695913faf3574bf18c586a343d1360a
skyscraper <kwikius@yahoo.com>
2015-12-22 10:12:39 +0000

    Modify reciver dipslay to show a small black box if the packet fails error checking

--- examples/osd_zapp4_rx/on_draw.cpp
+++ examples/osd_zapp4_rx/on_draw.cpp
@@ -2,38 +2,19 @@
 #include <cstdio>
 #include <quan/uav/osd/api.hpp>
 #include "rx_telemetry.hpp"
-//#include "resources.hpp"
 
-uint32_t get_good_packets_received();
-
-uint32_t get_packets_received();
+bool packet_good();
 
 namespace quan{ namespace uav { namespace osd{
 
    void on_draw()
    {
-      pxp_type pos{-150,40};
+      pxp_type pos{-150,10};
 
-      norm_position_type* gps_position_in = mutex_acquire_position(5);
-      if ( gps_position_in != nullptr){
-         norm_position_type gps_position = *gps_position_in;
-         mutex_release_position();
-         char buf[ 100];
-         sprintf(buf,"lat = %8.3f deg",static_cast<double>(gps_position.lat.numeric_value())* 1e-7);
-         draw_text(buf,pos);
-         pos.y -= 20;
+      if ( get_num_telem_errors() != 0){
+         clear_telem_errors();
+         draw_box(pos, pos + pxp_type{15,15}, colour_type::black, true);
 
-         sprintf(buf,"lon = %8.3f deg",static_cast<double>(gps_position.lon.numeric_value())*1e-7);
-         draw_text(buf,pos);
-         pos.y -= 20;
-
-         sprintf(buf,"alt = %8.3f m",static_cast<double>(gps_position.alt.numeric_value() *1e-3) );
-         draw_text(buf,pos);
-//         pos.y -= 20;
-//         unsigned const packets = get_packets_received();
-//         unsigned const good_packets = get_good_packets_received();
-//         sprintf(buf,"pkts = %u,gpkts = %u",packets, good_packets);
-//         draw_text(buf,pos);
       }
    }
 
