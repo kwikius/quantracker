2ca83e16e373bc5929b772f216864dbc4260d75a
Andy Little <kwikius@yahoo.com>
2016-10-01 20:07:13 +0100

    use lines not text for now since looks like font colours arent correct

--- examples/hello_world/main.cpp
+++ examples/hello_world/main.cpp
@@ -4,17 +4,23 @@ Minimal Quantracker Air OSD Hello World application
 
 #include <quan/stm32/gpio.hpp>
 #include <quantracker/osd/osd.hpp>
-#include <quantracker/air/osd/resources.hpp>
+#include "../../air/osd/resources.hpp"
 int main()
 {
    osd_setup();           
    create_draw_task();   
    quan::stm32::set<heartbeat_led_pin>(); 
+    quan::stm32::set<notify_led1>(); 
+    quan::stm32::set<notify_led2>(); 
    vTaskStartScheduler(); 
 }
 
 // your custom drawing function
 void quan::uav::osd::on_draw()
 {
-   draw_text("Hello World",{-60,0});
+   //draw_text("Hello World",{-60,0});
+    draw_line ( {-100,-100}, {100,100}, colour_type::black );
+    draw_line ( {-105,-100}, {95,100}, colour_type::white );
+    draw_line ( {-110,-100}, {90,100}, colour_type::grey );
+    draw_line ( {-115,-100}, {85,100}, colour_type::transparent );
 }
