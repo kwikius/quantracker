6bae56bdb0e3ff45a7126105d782c4c4376a1cd5
Andy Little <kwikius@yahoo.com>
2017-01-09 09:34:44 +0000

    correct typo in user output alt not lat and incr buffer size

--- examples/osd_zapp4_rx/output.cpp
+++ examples/osd_zapp4_rx/output.cpp
@@ -31,17 +31,17 @@ namespace {
    // do output human readable
     void do_output()
    {  
-      if ( mavlink_tx_rx_task::out_space_avail() >= 50){
+      if ( mavlink_tx_rx_task::out_space_avail() >= 60){
          quan::uav::osd::norm_position_type * pos_var = mutex_acquire_position(5);
          if ( pos_var != nullptr){
             quan::uav::osd::norm_position_type pos = *pos_var;
             mutex_release_position();
              
-            char buf[50];
-            snprintf(buf,50,"lat: % 10.5f, lon: % 10.5f, alt: % 8.2f m\n"
+            char buf[60];
+            snprintf(buf,60,"lat: % 10.5f, lon: % 10.5f, alt: % 10.2f m\n"
                ,pos.lat.numeric_value()/1.e7
                ,pos.lon.numeric_value()/1.e7
-               ,pos.lat.numeric_value()/1.e3
+               ,pos.alt.numeric_value()/1.e3
             );
             mavlink_tx_rx_task::write((const uint8_t*)buf,strlen(buf));
          }
