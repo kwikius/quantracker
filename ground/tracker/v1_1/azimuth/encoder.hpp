87fbb10013981d9e59b24158f02e7ffd6d1a0546
Andy Little <kwikius@yahoo.com>
2017-02-06 11:22:34 +0000

    make encoder function names more uniform

--- ground/tracker/v1_1/azimuth/encoder.hpp
+++ ground/tracker/v1_1/azimuth/encoder.hpp
@@ -1,6 +1,7 @@
 #ifndef QUANTRACKER_GROUND_V1_1_AZIMUTH_ENCODER_HPP_INCLUDED
 #define QUANTRACKER_GROUND_V1_1_AZIMUTH_ENCODER_HPP_INCLUDED
 
+#include <quan/angle.hpp>
 #include "../system/tracker_resources.hpp"
 
  struct azimuth_encoder{
@@ -10,18 +11,29 @@
       typedef  azimuth_quadrature_counter  counter;
      
       static uint32_t get_steps_per_revolution(){ return m_steps_per_revolution;}
-      static bool is_indexed() { return m_is_indexed;}
-      static uint32_t get_count() 
-      {
-         return counter::get()->cnt;
-      }
+
+      static bool is_indexed()  { return m_is_indexed;}
+      static uint32_t get_index() { return counter::get()->cnt; }
       static bool set_index( uint32_t value);
+
+      static uint32_t bearing_to_encoder(quan::angle::rad const & angle_in)
+      {
+         return static_cast<uint32_t>(
+            ( unsigned_modulo(angle_in) * azimuth_encoder::get_steps_per_revolution() ) / quan::angle::two_pi
+         );
+      }
+      // precondition: encoder_index is < azimuth_encoder::get_steps_per_revolution()
+      static quan::angle::rad encoder_to_bearing(uint32_t encoder_index)
+      {
+         return (encoder_index * quan::angle::two_pi) / azimuth_encoder::get_steps_per_revolution();
+      }
+
     private:
         
         static void un_set_index() { m_is_indexed = false;}
         static void set_steps_per_revolution(uint32_t val)
         { 
-           // check greater than 0. In fact check greater than some minimum e.g 360
+           // check greater than some minimum e.g 3 stesps per revolution
             m_steps_per_revolution = val;
         }
         friend struct      azimuth_servo;
