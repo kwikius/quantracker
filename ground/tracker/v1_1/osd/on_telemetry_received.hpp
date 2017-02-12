fd3a4b56a3cba67b7015e123ab6653b6f4e62ecf
Andy Little <kwikius@yahoo.com>
2017-02-12 17:52:47 +0000

    add a queue for on_telemtry received to send on

---
+++ ground/tracker/v1_1/osd/on_telemetry_received.hpp
@@ -0,0 +1,25 @@
+#ifndef QUANTRACKER_GROUND_TRACKER_V1_1_ON_TELEMETRY_RECEIVED_HPP_INCLUDED
+#define QUANTRACKER_GROUND_TRACKER_V1_1_ON_TELEMETRY_RECEIVED_HPP_INCLUDED
+
+/*
+ Copyright (c) 2017 Andy Little 
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include "FreeRTOS.h"
+
+QueueHandle_t get_vrx_telem_queue_handle();
+
+#endif // QUANTRACKER_GROUND_TRACKER_V1_1_ON_TELEMETRY_RECEIVED_HPP_INCLUDED
