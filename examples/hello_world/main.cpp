4b729d96c81c0fc7fecaf66641f7a2f2d7a40bc9
Andy Little <kwikius@yahoo.com>
2017-03-15 09:13:18 +0000

    add mwosd fonts to helloc world example

--- examples/hello_world/main.cpp
+++ examples/hello_world/main.cpp
@@ -2,37 +2,18 @@
 Minimal Quantracker Air OSD Hello World application
 */
 
-#include <quan/stm32/gpio.hpp>
+
 #include <quantracker/osd/osd.hpp>
-#include "../../air/osd/resources.hpp"
-#include "bitmaps.hpp"
 
 int main()
 {
    osd_setup();           
    create_draw_task();   
-   quan::stm32::set<heartbeat_led_pin>(); 
-#if defined QUAN_AERFLITE_BOARD
-    quan::stm32::set<notify_led1>(); 
-    quan::stm32::set<notify_led2>(); 
-#endif
    vTaskStartScheduler(); 
-}
-
-namespace {
- uint32_t draw_count = 0;
 }
 
 // your custom drawing function
 void quan::uav::osd::on_draw()
 {
-    draw_text("Hello World",{-60,30}); 
-    draw_line ( {-120,-100}, {80,100}, colour_type::white ); // white
-    draw_line ( {-100,-100}, {100,100}, colour_type::black ); // black
-    draw_bitmap(get_bitmap(BitmapID::home_arrow),{50,-50});
-
-    if ( ++draw_count == 50){
-       draw_count = 0;
-       quan::stm32::complement<heartbeat_led_pin>();
-    }
+   draw_text("Hello World",{-140,30});
 }
